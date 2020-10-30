08985f54aec9322aea86357346f74668f6967b37
Ransom Ahanmisi <60149483+Ransom8@users.noreply.github.com>
2020-10-30 16:46:53 +0100

    Finished major functions and UI

---
+++ app/src/main/java/com/example/map_app_week_8/Constant/LocationLogging.kt
@@ -0,0 +1,10 @@
+package com.example.map_app_week_8.Constant
+
+/**
+ * Here the data class is declare for coordinates longitude and latitude
+ */
+
+data class LocationLogging(
+    var Latitude: Double? = 0.0,
+    var Longitude: Double? = 0.0
+)
